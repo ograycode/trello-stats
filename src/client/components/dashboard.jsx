ef9b7ef77f76e6d6acde041cc3f692a2cba70a88
Jason O'Gray <jason@theograys.com>
2017-03-17 02:08:08 +0000

    Added a quick velocity calculation, will clean up later.

--- src/client/components/dashboard.jsx
+++ src/client/components/dashboard.jsx
@@ -5,8 +5,46 @@ import trelloState from "trello-state";
 import moment from "moment";
 /**/
 
-
 class Dashboard extends React.Component {
+  
+  constructor(props) {
+    super(props)
+    this.state = {
+      velocityStart: 1,
+      velocityEnd: this.days(),
+      velocityDoneList: null,
+      dailyData: []
+    };
+  }
+  
+  setVelocityStart(e) {
+    this.setState({velocityStart: e.target.value});
+  }
+  
+  setVelocityEnd(e) {
+    this.setState({velocityEnd: e.target.value});
+  }
+  
+  setVelocityDoneList(e) {
+    this.setState({velocityDoneList: e.target.value});
+  }
+  
+  days() {
+    return 60;
+  }
+  
+  calcVelocity(doneListId, {lists, cards}) {
+    if (!doneListId || !lists.length || !cards.length) {
+      return "Not enough data";
+    }
+    const data = this.calcCfdData({lists, cards});
+    const start = this.state.velocityStart;
+    const end = this.state.velocityEnd;
+    const firstDayCount = data[start - 1][doneListId];
+    const lastDayCount = data[end - 1][doneListId];
+    const weeks = (end - start) / 7.0;
+    return `${((lastDayCount - firstDayCount) / weeks).toFixed(2)} per week`;
+  }
 
   getStartDate(days) {
     const date = moment();
@@ -16,8 +54,12 @@ class Dashboard extends React.Component {
   }
 
   calcCfdData({lists, cards}) {
+    // quick hack to "cache" the data
+    if (this.state.dailyData.length > 0) {
+      return this.state.dailyData;
+    }
     const dashData = [];
-    const days = 60;
+    const days = this.days();
     const date = this.getStartDate(days);
     for (let num = 0; num <= days; num++) {
       date.add(1, "days");
@@ -31,6 +73,7 @@ class Dashboard extends React.Component {
       });
       dashData.push(node);
     }
+    this.setState({dailyData: dashData});
     return dashData;
   }
 
@@ -66,9 +109,54 @@ class Dashboard extends React.Component {
     const lists = this.props.lists;
     const dash = this.props.dash;
     const cards = this.props.cards;
+    const listOptions = lists.map((list) => {
+      return (<option value={list.id}>{list.name}</option>);
+    });
     return (
       <div className="ui container">
-        {this.createCfdChart({boards, lists, cards, dash})}
+        <div className="ui two column stackable grid">
+          <div className="column">
+            <div className="ui cards">
+              <div className="card">
+                <div className="content">
+                  <div className="header">
+                    Velocity
+                  </div>
+                  <div className="description">
+                    <div className="ui form">
+                      <div className="two fields">
+                        <div className="field">
+                          <label>Day Start</label>
+                          <input type="number"
+                            value={this.state.velocityStart}
+                            onChange={this.setVelocityStart.bind(this)}/>
+                        </div>
+                        <div className="field">
+                          <label>Day End</label>
+                          <input type="number"
+                            value={this.state.velocityEnd}
+                            onChange={this.setVelocityEnd.bind(this)}/>
+                        </div>
+                        <div className="field">
+                          <label>List</label>
+                          <select value={this.state.velocityDoneList} 
+                            onChange={this.setVelocityDoneList.bind(this)}>
+                            {listOptions}
+                          </select>
+                        </div>
+                      </div>
+                    </div>
+                    {this.calcVelocity(this.state.velocityDoneList, {lists, cards})}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        
+          <div className="sixteen wide column">
+            {this.createCfdChart({boards, lists, cards, dash})}
+          </div>
+        </div>
       </div>
     );
   }
